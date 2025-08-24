6e8d165edd6d2ecfec119626ecfe5a26db4b50f2
larscom <larskniep@gmail.com>
2025-08-24 12:41:48 +0200

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use image::{GenericImageView, Pixel};
-use std::error::Error;
+use std::{error::Error, fs};
 
 use wasm_bindgen::prelude::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file_path = "./image.png";
-    let img = image::open(file_path)?;
+    let img_bytes = fs::read(file_path)?;
+
+    let image = get_image(img_bytes.as_slice());
+    assert_eq!(image.width(), 800);
+    assert_eq!(image.height(), 600);
 
     Ok(())
 }
@@ -41,7 +45,7 @@ impl Image {
 }
 
 #[wasm_bindgen]
-pub fn get_pixels(image_bytes: &[u8]) -> Image {
+pub fn get_image(image_bytes: &[u8]) -> Image {
     let img = image::load_from_memory(image_bytes).expect("failed to load image");
     let data = img
         .pixels()
