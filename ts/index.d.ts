bd8bce6124028f3d8ce2ae23ba3e436db1ee7ba2
larscom <larskniep@gmail.com>
2025-08-24 21:15:00 +0200

    rm

--- ts/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Image {
-    width: number;
-    height: number;
-    data: ImageData;
-}
-export declare function load_from_url(url: RequestInfo): Promise<Image>;
-export declare function load_from_arraybuffer(buffer: ArrayBuffer): Promise<Image>;
