06f0d6126d72d38cdf196cc0d79f05d915184a64
larscom <larskniep@gmail.com>
2025-08-28 10:43:02 +0200

    update examples

--- examples/vite-react-ts/src/App.tsx
+++ examples/vite-react-ts/src/App.tsx
@@ -4,17 +4,18 @@ import './App.css'
 
 function App() {
   useEffect(() => {
-    load_from_url('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png').then(
-      (image) => {
-        const canvas = document.getElementById('canvas') as HTMLCanvasElement
-        canvas.width = image.width
-        canvas.height = image.height
+    ;(async () => {
+      const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png'
+      const image = await load_from_url(imageUrl)
 
-        const ctx = canvas.getContext('2d')!
+      const canvas = document.getElementById('canvas') as HTMLCanvasElement
+      canvas.width = image.width
+      canvas.height = image.height
 
-        ctx.putImageData(image.data, 0, 0)
-      }
-    )
+      const ctx = canvas.getContext('2d')!
+
+      ctx.putImageData(image.data, 0, 0)
+    })()
   }, [])
 
   return <canvas id="canvas"></canvas>
