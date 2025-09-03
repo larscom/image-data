63f447420d245adf596d05d878c8ec243dc2397e
larscom <larskniep@gmail.com>
2025-09-03 11:34:55 +0200

    move files

--- src/lib.rs
+++ src/lib.rs
@@ -46,3 +46,34 @@ pub fn get_image(image_bytes: &[u8]) -> Image {
 
     Image::new(img.width(), img.height(), data)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::get_image;
+
+    #[test]
+    fn test_png_image() {
+        let path = "./test_data/1_image.png";
+        let image_bytes = fs::read(path).unwrap();
+
+        let image = get_image(image_bytes.as_slice());
+
+        assert_eq!(image.width(), 800);
+        assert_eq!(image.height(), 600);
+        assert!(!image.data().is_empty());
+    }
+
+    #[test]
+    fn test_jpg_image() {
+        let path = "./test_data/2_image.jpg";
+        let image_bytes = fs::read(path).unwrap();
+
+        let image = get_image(image_bytes.as_slice());
+
+        assert_eq!(image.width(), 807);
+        assert_eq!(image.height(), 730);
+        assert!(!image.data().is_empty());
+    }
+}
